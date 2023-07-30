a67f101d3c2385db1dd8586faac15250363aa3f2
Rachamv <rkutalian@mail.com>
2023-07-30 13:14:09 +0100

    first step to success

--- backend/author/models.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.models import User
-from django.db import models
-
-class Author(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    # Add any additional fields you want for authors (e.g., bio, profile picture, etc.)
-
-    def __str__(self):
-        return self.user.username
